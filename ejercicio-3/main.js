3e36be711a4e09ab87a984def5ac6e01798dfde6
Rodri.V <veronrodrigo98@gmail.com>
2023-06-01 09:47:44 -0300

    Se completo la actividad 3

--- ejercicio-3/main.js
+++ ejercicio-3/main.js
@@ -1,7 +1,23 @@
 function limpiar(){
     document.getElementById("formulario").reset();
 }
-function sumar(){
+function suma(){
     let x = parseInt(document.getElementById("valor1").value);
     let y  = parseInt(document.getElementById("valor2").value);
+    document.getElementById('resultado').value=x+y;
+}
+function resta(){
+    let x = parseInt(document.getElementById("valor1").value);
+    let y  = parseInt(document.getElementById("valor2").value);
+    document.getElementById('resultado').value=x-y;
+}
+function multiplicacion(){
+    let x = parseInt(document.getElementById("valor1").value);
+    let y  = parseInt(document.getElementById("valor2").value);
+    document.getElementById('resultado').value=x*y;
+}
+function division(){
+    let x = parseInt(document.getElementById("valor1").value);
+    let y  = parseInt(document.getElementById("valor2").value);
+    document.getElementById('resultado').value=x/y;
 }
